c8af774456033ddd76140fe4a6dbd771a73585e8
Federica Gamba <federicagamba23@gmail.com>
2025-03-03 09:54:22 +0100

    removed trailing whitespace in sentence_en

--- scripts/print_structure.py
+++ scripts/print_structure.py
@@ -50,9 +50,8 @@ def print_structure(tree, sent_tree, umr, sent_num, output_file=None, print_in_f
         if sent_tree.lang != 'en':
             en_sent = [c for c in tree.comment.split('\n') if c.startswith(" text_en = ")]
             if en_sent:
-                print('Sentence Gloss (en):', f"{en_sent[0].lstrip(' text_en = ')}", '\n', file=destination)
-            else:
-                print(file=destination)
+                print('Sentence Gloss (en):', f"{en_sent[0].lstrip(' text_en = ')}", file=destination)
+            print(file=destination)
         else:
             print(file=destination)
         print('# sentence level graph:', file=destination)
@@ -69,7 +68,8 @@ def print_structure(tree, sent_tree, umr, sent_num, output_file=None, print_in_f
         if sent_tree.lang != 'en':
             en_sent = [c for c in tree.comment.split('\n') if c.startswith(" text_en = ")]
             if en_sent:
-                print('Sentence Gloss (en):', f"{en_sent[0].lstrip(' text_en = ')}", '\n', file=destination)
+                print('Sentence Gloss (en):', f"{en_sent[0].lstrip(' text_en = ')}", file=destination)
+                print(file=destination)
         print(f'Root: {sent_tree.root_var}', file=destination)
         print('Triples:', file=destination)
         for n in reorder_triples(sent_tree.triples):
